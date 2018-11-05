b6b506e8250078664bdefdcf7d9d380e950e3730
Mark Battersby <mark.alfred.battersby@gmail.com>
2018-11-05 12:56:28 -0500

    Handle case where rule shows before severity
    Thank you @suprMax !

--- linter.py
+++ linter.py
@@ -27,7 +27,7 @@ class Stylint(NodeLinter):
         # /path/to/file/example.styl
         ^.*$\s*
         # 177:24 colors warning hexidecimal color should be a variable
-        ^(?P<line>\d+):?(?P<col>\d+)?\s*((?P<warning>warning)|(?P<error>error))\s*(?P<message>.+)$\s*
+        ^(?P<line>\d+):?(?P<col>\d+)?\s*(?P<rule>\w+)?\s*((?P<warning>warning)|(?P<error>error))\s*(?P<message>.+)$\s*
     '''
     multiline = True
     error_stream = util.STREAM_STDOUT
